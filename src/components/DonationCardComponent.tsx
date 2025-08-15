a9aa377e97f98232f5e7f074e70bea3ef5f5c38c
Brent Kulwicki <37881163+brentkulwicki@users.noreply.github.com>
2025-08-15 10:09:36 -0400

    Fix/pre launch issues (#47)

--- src/components/DonationCardComponent.tsx
+++ src/components/DonationCardComponent.tsx
@@ -64,7 +64,7 @@ const LockSection = ({
               clipRule="evenodd"
             />
           </svg>
-          <span className="ml-1 sus-green-text font-medium">{endAmount}</span>
+          <span className="ml-1 sus-green-text font-medium">${endAmount}</span>
         </div>
       );
     case 'funded':
@@ -77,7 +77,7 @@ const LockSection = ({
           >
             <path d="M18 1.5c2.9 0 5.25 2.35 5.25 5.25v3.75a.75.75 0 0 1-1.5 0V6.75a3.75 3.75 0 1 0-7.5 0v3a3 3 0 0 1 3 3v6.75a3 3 0 0 1-3 3H3.75a3 3 0 0 1-3-3v-6.75a3 3 0 0 1 3-3h9v-3c0-2.9 2.35-5.25 5.25-5.25Z" />
           </svg>
-          <span className="ml-1 donation-card-black font-medium">{endAmount}</span>
+          <span className="ml-1 donation-card-black font-medium">${endAmount}</span>
         </div>
       );
     default:
@@ -94,7 +94,7 @@ const LockSection = ({
               clipRule="evenodd"
             />
           </svg>
-          <span className="ml-1 donation-card-locked font-medium">{endAmount}</span>
+          <span className="ml-1 donation-card-locked font-medium">${endAmount}</span>
         </div>
       );
   }
