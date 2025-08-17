91b8c9f56e258be4145332b53dbfb3f0ca3647bf
DanCoria <ifdanthencool@gmail.com>
2025-08-17 18:57:29 -0500

    Removed classes from portableTextSerializer.js children elements  and added them to the global.css file

--- src/lib/portableTextSerializer.js
+++ src/lib/portableTextSerializer.js
@@ -13,10 +13,10 @@ export function toPlainText(blocks = []) {
 
 const components = {
   block: {
-    normal: ({ children }) => `<p class="mb-4">${children}</p>`,
+    normal: ({ children }) => `<p>${children}</p>`,
     h1: ({ children }) => `${children}`,
-    h2: ({ children }) => `<h2 class="text-3xl mb-4">${children}</h2>`,
-    h3: ({ children }) => `<h3 class="text-2xl mb-4 font-bold">${children}</h3>`,
+    h2: ({ children }) => `<h2>${children}</h2>`,
+    h3: ({ children }) => `<h3>${children}</h3>`,
     // Fallback for unrecognized block styles
     default: ({ children, value }) => {
       console.warn(`Unrecognized block style: ${value.style}`);
@@ -24,16 +24,16 @@ const components = {
     },
   },
   list: {
-    bullet: ({ children }) => `<ul class="list-disc pl-5>${children}</ul>`,
+    bullet: ({ children }) => `<ul>${children}</ul>`,
   },
   listItem: {
-    bullet: ({ children }) => `<li class="mb-2">${children}</li>`,
+    bullet: ({ children }) => `<li>${children}</li>`,
   },
   marks: {
     textColor: ({ value, children }) => {
       return `<span style="color: ${value.value};">${children}</span>`;
     },
-    strong: ({ children }) => `<strong class="font-bold">${children}</strong>`,
+    strong: ({ children }) => `<strong>${children}</strong>`,
     em: ({ children }) => `<em>${children}</em>`,
     link: ({ mark, children }) => `<a href="${mark.href}">${children}</a>`,
   },
