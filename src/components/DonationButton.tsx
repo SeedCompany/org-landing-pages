e32dce5226af1ceec0bad39753b3ad868bd3e093
Brent Kulwicki <37881163+brentkulwicki@users.noreply.github.com>
2025-08-20 10:48:28 -0400

    fixes the button color and hover color on donation form (#66)
    fixes the give form button colors

--- src/components/DonationButton.tsx
+++ src/components/DonationButton.tsx
@@ -14,7 +14,7 @@ export const DonationButton = ({
   return (
     <button
       disabled={isSubmitting}
-      className={`${className} relative disabled:text-gray-300 disabled:hover:bg-emerald-600 disabled:hover:text-gray-300 h-14 donation-button m-2 rounded-sm px-3.5 py-2.5 text-sm font-semibold  ${buttonType === 'primary' ? 'bg-emerald-600 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600' : 'border-black border hover:text-gray-800 hover:border-gray-800 hover:bg-emerald-50'}`}
+      className={`${className} relative disabled:text-gray-300 disabled:hover:bg-emerald-600 disabled:hover:text-gray-300 h-14 donation-button m-2 rounded-sm px-3.5 py-2.5 text-sm font-semibold  ${buttonType === 'primary' ? 'bg-watermarkGreen text-white shadow-sm hover:bg-emerald-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600' : 'border-black border hover:text-gray-800 hover:border-gray-800 hover:bg-emerald-50'}`}
       {...rest}
     >
       {children}
