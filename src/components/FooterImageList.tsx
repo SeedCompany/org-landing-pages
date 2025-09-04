8605251fc6a218b16801ed2dd956834813f150d9
DanCoria <ifdanthencool@gmail.com>
2025-09-04 12:05:59 -0500

    Update footer component with contact links, privacy policy, and updated logos

--- src/components/FooterImageList.tsx
+++ src/components/FooterImageList.tsx
@@ -13,18 +13,18 @@ interface Props {
 }
 
 const DEFAULT_FOOTER_IMAGES = [
-    {image: 'https://cdn.sanity.io/media-libraries/ml0ZDygBMJD9/images/ba39018fed274bf6dd7d60dc544efb2835b32eb8-150x150.png', altText: 'EIG-Certified'},
-    {image: 'https://cdn.sanity.io/media-libraries/ml0ZDygBMJD9/images/2ce55eebe9c33530fcccc4c0312084dea94c9c26-150x150.png', altText: 'Candid Gold Transparency 2024'},
-    {image: 'https://cdn.sanity.io/media-libraries/ml0ZDygBMJD9/images/26db7532e2720e74d581f3158025aa6ec8abd3f7-150x150.png', altText: 'ECFA Accredited'},
+    // {image: 'https://cdn.sanity.io/media-libraries/ml0ZDygBMJD9/images/ba39018fed274bf6dd7d60dc544efb2835b32eb8-150x150.png', altText: 'EIG-Certified'},
+    // {image: 'https://cdn.sanity.io/media-libraries/ml0ZDygBMJD9/images/2ce55eebe9c33530fcccc4c0312084dea94c9c26-150x150.png', altText: 'Candid Gold Transparency 2024'},
+    {image: 'https://cdn.sanity.io/media-libraries/ml0ZDygBMJD9/images/f77c1d6a02d8a7fdc8066a3e8bf5b73da6e15df9-450x375.png', altText: 'ECFA Accredited'},
 ]
 
 const FooterImageList: React.FC<Props> = ({ images = DEFAULT_FOOTER_IMAGES }) => {
   return (
-    <div className="flex flex-row gap-x-8 w-full items-center min-h-[150px] justify-center">
+    <div className="flex flex-row gap-x-8 w-full items-center min-h-[150px] justify-end">
       {images.map((imageItem, index) => (
-        <div key={index}>
+        <div className="flex md:justify-end" key={index}>
           {imageItem.image && (
-            <img src={imageItem.image} alt={imageItem.altText} />
+            <img className="w-1/3" src={imageItem.image} alt={imageItem.altText} />
           )}
         </div>
       ))}
