a4750675542dd5531975a60b46d02eaee72d9a7c
Seth McKnight <7377802+sethmcknight@users.noreply.github.com>
2025-09-17 14:02:30 -0600

    Add Sanity Media Plugin to view media library in Sanity Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
 import { simplerColorInput } from 'sanity-plugin-simpler-color-input';
+import { media } from 'sanity-plugin-media';
 import { schemaTypes } from './src/schemaTypes';
 
 const env = typeof process !== 'undefined' ? process.env : import.meta.env;
@@ -9,7 +10,7 @@ const env = typeof process !== 'undefined' ? process.env : import.meta.env;
 export default defineConfig({
   projectId: env.PUBLIC_SANITY_PROJECT_ID,
   dataset: env.PUBLIC_SANITY_DATASET,
-  plugins: [structureTool(), visionTool(), simplerColorInput()],
+  plugins: [structureTool(), visionTool(), simplerColorInput(), media()],
 
   schema: {
     types: schemaTypes,
