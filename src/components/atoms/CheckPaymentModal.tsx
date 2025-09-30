1a10a586e6c14658792af31a3fdfb28845815599
brentkulwicki <brentkulwicki@gmail.com>
2025-09-30 09:28:23 -0400

    fixes give by check modal on mobile

--- src/components/atoms/CheckPaymentModal.tsx
+++ src/components/atoms/CheckPaymentModal.tsx
@@ -14,7 +14,7 @@ export const CheckPaymentModal = ({
           className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
         />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
             <DialogPanel
               transition
               className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
@@ -32,7 +32,8 @@ export const CheckPaymentModal = ({
                   </DialogTitle>
                   <div className="mt-2 gotham">
                     <p className="text-left text-gray-900">
-                      Please make your check payable to Seed Company and send to the following address:
+                      Please make your check payable to Seed Company and send to the following
+                      address:
                     </p>
                     <p className="text-sm text-left mt-2 mb-3 text-gray-900 ml-2">
                       Seed Company
