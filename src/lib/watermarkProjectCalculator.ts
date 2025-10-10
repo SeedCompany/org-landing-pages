b079f4f2d5b65b9cdce8239996a9c19f776c2614
Brent Kulwicki <37881163+brentkulwicki@users.noreply.github.com>
2025-10-10 13:10:45 -0400

    updated the people group totals (#150)

--- src/lib/watermarkProjectCalculator.ts
+++ src/lib/watermarkProjectCalculator.ts
@@ -5,12 +5,12 @@ interface Impact {
 export const watermarkProjectCalculator = (amount: number): Impact => {
   const projectMarkers = [
     { total: 100000, peopleHelpedTotal: 332200, projectsUnlockedTotal: 4 },
-    { total: 250000, peopleHelpedTotal: 1134500, projectsUnlockedTotal: 7 },
-    { total: 500000, peopleHelpedTotal: 2435000, projectsUnlockedTotal: 11 },
-    { total: 800000, peopleHelpedTotal: 2493158, projectsUnlockedTotal: 15 },
-    { total: 1000000, peopleHelpedTotal: 3691458, projectsUnlockedTotal: 16 },
-    { total: 1500000, peopleHelpedTotal: 7043858, projectsUnlockedTotal: 18 },
-    { total: 2000000, peopleHelpedTotal: 12471597, projectsUnlockedTotal: 22 },
+    { total: 250000, peopleHelpedTotal: 1134500, projectsUnlockedTotal: 9 },
+    { total: 500000, peopleHelpedTotal: 2435000, projectsUnlockedTotal: 19 },
+    { total: 800000, peopleHelpedTotal: 2493158, projectsUnlockedTotal: 25 },
+    { total: 1000000, peopleHelpedTotal: 3691458, projectsUnlockedTotal: 30 },
+    { total: 1500000, peopleHelpedTotal: 7043858, projectsUnlockedTotal: 43 },
+    { total: 2000000, peopleHelpedTotal: 12471597, projectsUnlockedTotal: 51 },
   ];
   const currentProjectIndex = projectMarkers.findIndex((marker) => amount < marker.total);
   if (currentProjectIndex === 0) {
