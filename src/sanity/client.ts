1042ac0771710e01b5daaf8aef2a9d5e90471296
Carson Full <carson_full@tsco.org>
2025-10-27 18:35:04 -0500

    Move image builder into our facade client

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,6 +1,10 @@
 import type { ClientReturn } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import type { QueryParams } from 'sanity';
 import { sanityClient as client } from 'sanity:client';
+
+const imageBuilder = imageUrlBuilder(client);
 
 export const sanityClient = {
   fetch: <Groq extends string>(
@@ -9,4 +13,5 @@ export const sanityClient = {
   ): Promise<ClientReturn<Groq, unknown>> => {
     return client.fetch(query, params);
   },
+  image: (source: SanityImageSource) => imageBuilder.image(source),
 };
