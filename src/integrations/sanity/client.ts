47252ccd2fc4bdabb419606cb4f56d3f03c56280
Carson Full <carson_full@tsco.org>
2025-11-01 09:43:12 -0500

    Switch to extend sanity client with proxy
    Object.create() doesn't work with native private properties.

--- src/integrations/sanity/client.ts
+++ src/integrations/sanity/client.ts
@@ -10,6 +10,18 @@ const baseClient = _configuredClient as unknown as SanityClient;
 
 const imageBuilder = imageUrlBuilder(baseClient);
 
-export const sanityClient = Object.assign(Object.create(baseClient) as SanityClient, {
+const extra = {
   image: (source: SanityImageSource) => imageBuilder.image(source),
-});
+};
+
+export const sanityClient = new Proxy(baseClient, {
+  get(_, p) {
+    if (p in extra) {
+      return extra[p as keyof typeof extra];
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const out = Reflect.get(baseClient, p);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    return typeof out === 'function' ? out.bind(baseClient) : out;
+  },
+}) as SanityClient & typeof extra;
