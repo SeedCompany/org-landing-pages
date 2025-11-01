eabd80d6b5d609521357541977a777856fa71cbf
Carson Full <carson_full@tsco.org>
2025-11-01 08:48:42 -0500

    Fix precommit check not filtering to staged files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,9 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
-  // auto fix & format, but don't block the commit
-  '**/*.{js,mjs,ts,tsx,astro}': `sh -c 'LINT_STAGED=1 yarn eslint --fix "$@" || true'`,
+  '**/*.{js,mjs,ts,tsx,astro}': (files) => {
+    // auto fix & format
+    const cmd = `LINT_STAGED=1 yarn eslint --fix ${files.join(' ')}`;
+    // but don't block the commit
+    return `sh -c '${cmd} || true'`;
+  },
 };
