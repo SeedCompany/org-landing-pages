60af95cfb266e6b8752085d9e1372824ce997921
Carson Full <carson_full@tsco.org>
2025-11-01 08:36:20 -0500

    Speed up precommit hook by disabling the worst performing lint rules without fixers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import { env } from 'node:process';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
@@ -31,4 +32,25 @@ export default tseslint.config(
     // https://github.com/ota-meshi/eslint-plugin-astro/issues/447
     extends: [tseslint.configs.disableTypeChecked],
   },
+  // Adjust rules when running in the commit hook
+  env.LINT_STAGED
+    ? {
+        // Disable the worst performers without fixers
+        rules: {
+          '@typescript-eslint/no-misused-promises': 'off',
+          '@typescript-eslint/no-unsafe-assignment': 'off',
+          '@typescript-eslint/no-unused-vars': 'off',
+          '@typescript-eslint/no-unsafe-argument': 'off',
+          '@typescript-eslint/no-unsafe-return': 'off',
+          '@typescript-eslint/no-floating-promises': 'off',
+          '@typescript-eslint/unbound-method': 'off',
+          '@typescript-eslint/no-base-to-string': 'off',
+          'no-redeclare': 'off',
+          '@typescript-eslint/no-unsafe-member-access': 'off',
+          'no-global-assign': 'off',
+          '@typescript-eslint/no-unsafe-call': 'off',
+          '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+        },
+      }
+    : [],
 );
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
   // auto fix & format, but don't block the commit
-  '**/*.{js,mjs,ts,tsx,astro}': `sh -c 'yarn eslint --fix "$@" || true'`,
+  '**/*.{js,mjs,ts,tsx,astro}': `sh -c 'LINT_STAGED=1 yarn eslint --fix "$@" || true'`,
 };
