89a7819b6467f3aad26c8d8a96033afa44ac0acc
Carson Full <carson_full@tsco.org>
2025-11-03 16:57:18 -0600

    Add Park UI: Button, Loader, Spinner, Group

--- src/theme/recipes/index.ts
+++ src/theme/recipes/index.ts
@@ -1,2 +1,12 @@
-export const recipes = {};
+import { absoluteCenter } from './absolute-center.ts';
+import { button } from './button.ts';
+import { group } from './group.ts';
+import { spinner } from './spinner.ts';
+
+export const recipes = {
+  absoluteCenter,
+  button,
+  group,
+  spinner,
+};
 export const slotRecipes = {};
