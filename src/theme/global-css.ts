e944dc10ccdfdcb924c83ad1e31b491b50299ba2
Carson Full <carson_full@tsco.org>
2025-11-03 16:41:03 -0600

    Init Park UI theme

---
+++ src/theme/global-css.ts
@@ -0,0 +1,17 @@
+import { defineGlobalStyles } from '@pandacss/dev';
+
+export const globalCss = defineGlobalStyles({
+  extend: {
+    html: {
+      '--global-color-border': 'colors.border',
+      '--global-color-focus-ring': 'colors.colorPalette.solid.bg',
+      '--global-color-placeholder': 'colors.fg.subtle',
+      '--global-color-selection': 'colors.colorPalette.subtle.bg',
+      colorPalette: 'gray',
+    },
+    body: {
+      background: 'gray.1',
+      color: 'fg.default',
+    },
+  },
+});
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,10 +1,92 @@
 import { type ExtendableOptions, type PartialTheme } from '@pandacss/types';
+import { animationStyles } from './animation-styles.ts';
+import { green } from './colors/green.ts';
+import { red } from './colors/red.ts';
+import { slate } from './colors/slate.ts';
+import { conditions } from './conditions.ts';
+import { globalCss } from './global-css.ts';
+import { keyframes } from './keyframes.ts';
+import { layerStyles } from './layer-styles.ts';
+import { recipes, slotRecipes } from './recipes';
+import { textStyles } from './text-styles.ts';
+import { colors } from './tokens/colors.ts';
+import { durations } from './tokens/durations.ts';
+import { shadows } from './tokens/shadows.ts';
+import { zIndex } from './tokens/z-index.ts';
 
-const theme = {} satisfies PartialTheme;
+const theme = {
+  animationStyles: animationStyles,
+  recipes: recipes,
+  slotRecipes: slotRecipes,
+  keyframes: keyframes,
+  layerStyles: layerStyles,
+  textStyles: textStyles,
+
+  tokens: {
+    colors,
+    durations,
+    zIndex,
+  },
+
+  semanticTokens: {
+    colors: {
+      fg: {
+        default: {
+          value: {
+            _light: '{colors.gray.12}',
+            _dark: '{colors.gray.12}',
+          },
+        },
+
+        muted: {
+          value: {
+            _light: '{colors.gray.11}',
+            _dark: '{colors.gray.11}',
+          },
+        },
+
+        subtle: {
+          value: {
+            _light: '{colors.gray.10}',
+            _dark: '{colors.gray.10}',
+          },
+        },
+      },
+      border: {
+        value: {
+          _light: '{colors.gray.4}',
+          _dark: '{colors.gray.4}',
+        },
+      },
+      error: {
+        value: {
+          _light: '{colors.red.9}',
+          _dark: '{colors.red.9}',
+        },
+      },
+
+      green,
+      red,
+      // Park UI requires a gray color available
+      gray: slate,
+    },
+
+    shadows,
+
+    radii: {
+      l1: { value: '{radii.xs}' },
+      l2: { value: '{radii.sm}' },
+      l3: { value: '{radii.md}' },
+    },
+  },
+} satisfies PartialTheme;
 
 export const setupTheme = () =>
   ({
     theme: {
       extend: theme,
     },
+
+    globalCss: globalCss,
+    conditions: conditions,
   }) satisfies ExtendableOptions;
