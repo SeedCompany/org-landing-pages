a6d2ca275875e1f1be337c6a23475a6e3a1755ec
Carson Full <carson_full@tsco.org>
2025-11-04 09:55:58 -0600

    Start defining semantic typography/text styles

--- src/theme/text-styles.ts
+++ src/theme/text-styles.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { defineTextStyles } from '@pandacss/dev';
 
 export const textStyles = defineTextStyles({
@@ -13,4 +12,52 @@ export const textStyles = defineTextStyles({
   '5xl': { value: { fontSize: '5xl', lineHeight: '3.75rem', letterSpacing: '-0.02em' } },
   '6xl': { value: { fontSize: '6xl', lineHeight: '4.5rem', letterSpacing: '-0.02em' } },
   '7xl': { value: { fontSize: '7xl', lineHeight: '5.75rem', letterSpacing: '-0.02em' } },
+
+  title1: {
+    description: 'Page Titles. It should be used sparingly.',
+    value: {
+      fontFamily: '{fonts.rader}',
+      fontSize: { base: '5xl', sm: '7xl' },
+      letterSpacing: '-0.02em',
+      textWrap: 'balance',
+      textTransform: 'uppercase',
+    },
+  } as const,
+
+  // TODO sizing??
+
+  header1: {
+    description: 'Big Headers.',
+    value: {
+      fontFamily: '{fonts.serif}',
+    },
+  },
+  header2: {
+    value: {
+      fontFamily: '{fonts.sans}',
+    },
+  },
+  header4: {
+    value: {
+      fontFamily: '{fonts.mono}',
+      fontWeight: '{fontWeights.semibold}',
+    },
+  },
+
+  body1: {
+    value: {
+      fontFamily: '{fonts.sans}',
+    },
+  },
+  body2: {
+    value: {
+      fontFamily: '{fonts.sans}',
+      fontSize: 'sm',
+    },
+  },
+  body3: {
+    value: {
+      fontFamily: '{fonts.mono}',
+    },
+  },
 });
