fd2c6bb709f39c57f6e05b791af61fda2445805c
Carson Full <carson_full@tsco.org>
2025-11-11 17:32:17 -0600

    Temp page to view form

--- src/components/DonationFormIsland.tsx
+++ src/components/DonationFormIsland.tsx
@@ -1,9 +1,18 @@
 import type { ComponentProps as PropsOf } from 'react';
 import { RecaptchaProvider } from '~/recaptcha';
 import { StripeProvider } from '~/stripe';
-import { DonationForm } from './DonationForm.tsx';
+import { DonationForm as DonationFormOld } from './DonationForm.tsx';
+import { DonationForm } from '~/features/donate/DonationForm.tsx';
 
-export const DonationFormIsland = (props: PropsOf<typeof DonationForm>) => (
+export const DonationFormIsland = (props: PropsOf<typeof DonationFormOld>) => (
+  <StripeProvider>
+    <RecaptchaProvider>
+      <DonationFormOld {...props} />
+    </RecaptchaProvider>
+  </StripeProvider>
+);
+
+export const DonationFormIsland2 = (props: PropsOf<typeof DonationForm>) => (
   <StripeProvider>
     <RecaptchaProvider>
       <DonationForm {...props} />
