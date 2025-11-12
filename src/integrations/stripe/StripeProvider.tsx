aa7cd8d74a4e77f06be8fd667037446f5a67a29f
Carson Full <carson_full@tsco.org>
2025-11-12 13:12:32 -0600

    Customize Stripe to use our sans font stack

--- src/integrations/stripe/StripeProvider.tsx
+++ src/integrations/stripe/StripeProvider.tsx
@@ -11,6 +11,36 @@ const elementsOptions: StripeElementsOptions = {
   mode: 'payment',
   amount: 7500,
   currency: 'usd',
+  appearance: {
+    variables: {
+      fontFamily: [
+        `'Montserrat'`,
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ].join(', '),
+    },
+  },
+  fonts: [
+    {
+      // This seems like the easiest way to get the font to load 3rd party.
+      // Since currently we bundle the font & don't have hosted static url we can use.
+      // or even a dynamic one we can reference here & have it work in vite dev.
+      // This does mean that the user will have to load two different versions of this font.
+      cssSrc: 'https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap',
+    },
+  ],
 };
 
 export const StripeProvider = ({ children }: ChildrenProp) => {
