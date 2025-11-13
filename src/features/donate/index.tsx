50ed1cf22fc9a8076d1fae78bb13c8fcbff0c467
Carson Full <carson_full@tsco.org>
2025-11-12 23:49:57 -0600

    Add donate form main entrypoint / with provider wrappers

---
+++ src/features/donate/index.tsx
@@ -0,0 +1,12 @@
+import type { ComponentProps as PropsOf } from 'react';
+import { DonationForm as Form } from './DonationForm.tsx';
+import { StripeProvider } from '~/stripe';
+import { RecaptchaProvider } from '~/recaptcha';
+
+export const DonationForm = (props: PropsOf<typeof Form>) => (
+  <StripeProvider>
+    <RecaptchaProvider>
+      <Form {...props} />
+    </RecaptchaProvider>
+  </StripeProvider>
+);
