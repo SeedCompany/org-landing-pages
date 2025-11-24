8ac79e5eac5af28ff53c536220df7732c2ab2bca
Seth McKnight <7377802+sethmcknight@users.noreply.github.com>
2025-11-24 13:58:15 -0700

    chore: remove files and directories ignored by .gitignore

--- generated/styled-system/recipes/index.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export * from './absolute-center';
-export * from './accordion';
-export * from './button';
-export * from './group';
-export * from './spinner';
--- generated/styled-system/recipes/index.mjs
+++
@@ -1,5 +0,0 @@
-export * from './absolute-center.mjs';
-export * from './accordion.mjs';
-export * from './button.mjs';
-export * from './group.mjs';
-export * from './spinner.mjs';
