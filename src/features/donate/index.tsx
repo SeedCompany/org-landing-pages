030d8cdb155c4c8581808440462f7ed723b11d95
brentkulwicki <brentkulwicki@gmail.com>
2026-05-06 14:31:35 -0400

    fixes an issue with the GTL donation form

--- src/features/donate/index.tsx
+++ src/features/donate/index.tsx
@@ -1,3 +1,4 @@
+import { many } from '@seedcompany/common';
 import type { ComponentProps as PropsOf } from 'react';
 import { DonationForm as Form } from './DonationForm.tsx';
 import { StripeProvider } from '~/stripe';
@@ -5,10 +6,14 @@ import { RecaptchaProvider } from '~/recaptcha';
 
 export { getDonationProps } from './sanity';
 
-export const DonationForm = (props: PropsOf<typeof Form>) => (
-  <StripeProvider>
-    <RecaptchaProvider>
-      <Form {...props} />
-    </RecaptchaProvider>
-  </StripeProvider>
-);
+export const DonationForm = (props: PropsOf<typeof Form>) => {
+  const initialCadence =
+    (props.cadence?.options ? many(props.cadence.options) : undefined)?.at(0) ?? 'OneTime';
+  return (
+    <StripeProvider initialMode={initialCadence === 'Monthly' ? 'subscription' : 'payment'}>
+      <RecaptchaProvider>
+        <Form {...props} />
+      </RecaptchaProvider>
+    </StripeProvider>
+  );
+};
